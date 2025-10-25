10d6f4a7abc71b4185f7f8f7a6dea531ecab5e7f
Rita Cheng <718216+metermaid@users.noreply.github.com>
2025-10-24 20:10:08 -0400

    switch to pytesseract because im running this in vmware lol

--- core/ocr.py
+++ core/ocr.py
@@ -1,24 +1,19 @@
-import easyocr
+import pytesseract
 from PIL import Image
 import numpy as np
 import re
-
-reader = easyocr.Reader(["en"], gpu=False)
+import logging
 
 def extract_text(pil_img: Image.Image) -> str:
   img_np = np.array(pil_img)
-  result = reader.readtext(img_np)
-  texts = [text[1] for text in result]
-  return " ".join(texts)
+  result = pytesseract.image_to_string(img_np)
+  result = re.sub(r"\n", " ", result)
+  return result
 
 def extract_number(pil_img: Image.Image) -> int:
   img_np = np.array(pil_img)
-  result = reader.readtext(img_np, allowlist="0123456789")
-  texts = [text[1] for text in result]
-  joined_text = "".join(texts)
-
-  digits = re.sub(r"[^\d]", "", joined_text)
-
+  result = pytesseract.image_to_string(img_np, config= "-c tessedit_char_whitelist=0123456789")
+  digits = re.sub(r"[^\d]", "", result)
   if digits:
     return int(digits)
   
